09eee9ce388afa03d5ee7104a4757e8116f70e05
unknown <you@example.com>
2024-12-13 00:40:30 +0100

    fixed covarage report

--- strajk-bowling/vite.config.js
+++ strajk-bowling/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTest.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "lcov", "json-summary"],
+      include: ["src/**/*.js", "src/**/*.jsx"],
+      exclude: ["src/main.jsx"],
+    },
+  },
+});
